4f9fbd8d86fe228f560b04dc7f946c6ce142f327
thlanza <thlanza@hotmail.com>
2019-11-13 18:43:10 -0200

    agora o programa está em html puro sem react. na lição do javascript

---
+++ shared.js
@@ -0,0 +1,19 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
+
+var no = document.querySelector('.modal__action--negative');
+console.log(no);
+no.addEventListener('click', function() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+});
+
