e89d9fa43f8e7d1857bca05596598bca690f67f6
Thiago Lanza <thlanza@hotmail.com>
2019-11-18 22:39:08 -0300

    mais uma aula de javascript no css

--- shared.js
+++ shared.js
@@ -1,19 +1,44 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var no = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
 
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open' -> This overrides
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-var no = document.querySelector('.modal__action--negative');
-console.log(no);
-no.addEventListener('click', function() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
-});
+backdrop.addEventListener('click', function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+})
+
+if (no) {
+no.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+};
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+
+})
 
